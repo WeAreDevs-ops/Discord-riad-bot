89074a19f6c6930dfb46c19a250ad5776d4912e0
Niqqas <cleencuevas@gmail.com>
2026-01-30 16:19:07 +0800

    Update client ID for application commands

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 (async () => {
     try {
         console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationCommands(client.user?.id ?? 'YOUR_CLIENT_ID'), { body: commands });
+        await rest.put(Routes.applicationCommands(client.user?.id ?? '1425782268403646518'), { body: commands });
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
